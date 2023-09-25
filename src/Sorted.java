bb261ebb8ca1ac2dfd00d9301f797f0751e8d368
M7mdknh <132266454+M7mdknh@users.noreply.github.com>
2023-09-26 00:11:29 +0300

    Update Sorted.java

--- src/Sorted.java
+++ src/Sorted.java
@@ -1,17 +1,16 @@
 public class Sorted {
-    public static void selectionSort(int[] arr) {
+    public static void selectionSort(String[] arr) {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
-
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j].compareToIgnoreCase(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
 
-            int temp = arr[minIndex];
+            String temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
